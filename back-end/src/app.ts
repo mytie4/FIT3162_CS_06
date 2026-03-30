5cac380e073d988e94a201d06ef9ec7f757d56bd
bia3334 <quankun3357@gmail.com>
2026-03-30 23:04:36 +0000

    Fix cors

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,10 +1,10 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express from 'express';
-import swaggerUi from 'swagger-ui-express';
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import swaggerUi from "swagger-ui-express";
 
-import apiRouter from './routes';
-import swaggerSpec from './swagger';
+import apiRouter from "./routes";
+import swaggerSpec from "./swagger";
 
 dotenv.config();
 
@@ -12,25 +12,26 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL ?? 'http://localhost:5173',
+    origin: true,
+    credentials: true,
   }),
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (_req, res) => {
+app.get("/", (_req, res) => {
   res.json({
-    message: 'Backend API is running',
+    message: "Backend API is running",
   });
 });
 
-app.get('/api-docs.json', (_req, res) => {
-  res.setHeader('Content-Type', 'application/json');
+app.get("/api-docs.json", (_req, res) => {
+  res.setHeader("Content-Type", "application/json");
   res.send(swaggerSpec);
 });
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
 export default app;
